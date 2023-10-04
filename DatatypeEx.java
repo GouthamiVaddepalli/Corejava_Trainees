13243e081a815c555f22b056e8b7311ab88d024c
GouthamiVaddepalli <144352304+GouthamiVaddepalli@users.noreply.github.com>
2023-10-04 17:32:40 +0530

    Add files via upload

---
+++ DatatypeEx.java
@@ -0,0 +1,27 @@
+package corejava;
+
+public class DatatypeEx {
+
+	public static void main(String[] args) {
+
+		byte b = 15; // -128 to 127
+
+		short s = 20000; // -32768to 32767
+
+		int i = 2144566;
+
+		long contact = 9845623L;
+
+		char c = 'f';
+
+		boolean bool = true;
+
+		float f = 2.523333f;// end with f
+
+		double d = 30.5;
+
+		System.out.println(b + "  " + s + " " + i + "  " + c + " " + bool + " " + f + "  " + d + " " + contact);
+
+	}
+
+}
