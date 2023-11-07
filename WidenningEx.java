f1a9498eb4c38a226cf5c0ec0beaa1f7062fd5da
GouthamiVaddepalli <144352304+GouthamiVaddepalli@users.noreply.github.com>
2023-11-07 19:44:48 +0530

    Add files via upload

---
+++ WidenningEx.java
@@ -0,0 +1,17 @@
+package corejava;
+
+public class WidenningEx {
+
+	public static void main(String[] args) { // widenningEx casting
+		int i = 100;
+		float f = i;
+		System.out.println(i);// ctrl+space
+		System.out.println(f);
+		byte b = 45;
+		float d = b;
+		System.out.println(b);
+		System.out.println(d);
+
+	}
+
+}
