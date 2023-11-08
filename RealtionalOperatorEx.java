9d8e77760662b21c9e3ea58cd4821b2b8a315e69
GouthamiVaddepalli <144352304+GouthamiVaddepalli@users.noreply.github.com>
2023-11-08 12:24:25 +0530

    Add files via upload

---
+++ RealtionalOperatorEx.java
@@ -0,0 +1,11 @@
+package com.traines.corejava;
+
+public class RealtionalOperatorEx {
+
+	public static void main(String[] args) {
+		int a= 100;
+		int b = 50;
+		System.out.println(a != b );
+	}
+
+}
