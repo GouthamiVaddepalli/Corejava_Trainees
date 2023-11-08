1c00ce3fe1a1306eec1a5f06646e9bff0bd9d13e
GouthamiVaddepalli <144352304+GouthamiVaddepalli@users.noreply.github.com>
2023-11-08 12:23:30 +0530

    Add files via upload

---
+++ ArthameticOperatosEx.java
@@ -0,0 +1,18 @@
+package com.traines.corejava;
+
+public class ArthameticOperatosEx {
+
+	public static void main(String[] args) { // mathametical operations
+		int a = 20;
+		int b = 30;
+		int addition = a + b;
+		int subtraction = a-b;
+		System.out.println("addition is : " + addition); // addition
+		System.out.println("subtraction is : " + subtraction);// subtraction
+		System.out.println(a/b); //division
+		System.out.println(b%a);
+		System.out.println(b*a); // mutiplication
+
+	}
+
+}
