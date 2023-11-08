5781cdb84f55f21f5153ae28d33de76e5a0c9e1a
GouthamiVaddepalli <144352304+GouthamiVaddepalli@users.noreply.github.com>
2023-11-08 12:24:41 +0530

    Add files via upload

---
+++ TernaryEx.java
@@ -0,0 +1,12 @@
+package com.traines.corejava;
+
+public class TernaryEx {
+
+	public static void main(String[] args) { // ternary operator
+		int a = 10;
+		int b = 5;
+		int min = (a>b)?a:b;
+		System.out.println(min);
+	}
+
+}
