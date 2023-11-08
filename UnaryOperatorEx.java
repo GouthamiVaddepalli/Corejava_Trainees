0fee96cd5479fe72861891dcff2cf07905194082
GouthamiVaddepalli <144352304+GouthamiVaddepalli@users.noreply.github.com>
2023-11-08 12:25:01 +0530

    Add files via upload

---
+++ UnaryOperatorEx.java
@@ -0,0 +1,19 @@
+package com.traines.corejava;
+
+public class UnaryOperatorEx {
+
+	public static void main(String[] args) { // unary operator
+		int a = 20;
+		int b = 10;
+		System.out.println(a++);//20 + 1 = 21
+		
+		System.out.println(++a);//21+1  =22
+		
+		System.out.println(b--); //10-1=9 
+		System.out.println(b); // 9
+		
+		System.out.println(--b);//9-1 = 8
+
+	}
+
+}
