838cbd67d80c65ae59ac81709496ab637ca97b92
GouthamiVaddepalli <144352304+GouthamiVaddepalli@users.noreply.github.com>
2023-11-08 20:54:46 +0530

    Add files via upload

---
+++ SimpleIfEx.java
@@ -0,0 +1,15 @@
+package com.traines.corejava;
+
+public class SimpleIfEx {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		if( b>a) {
+			
+		System.out.println("b is greater than a " );	
+	   }
+
+	}
+
+}
