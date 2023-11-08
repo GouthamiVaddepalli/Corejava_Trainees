49a3f4d2122041ee6402be2763fe76eba884db4f
GouthamiVaddepalli <144352304+GouthamiVaddepalli@users.noreply.github.com>
2023-11-08 12:24:04 +0530

    Add files via upload

---
+++ AssignmentOperatorEx.java
@@ -0,0 +1,22 @@
+package com.traines.corejava;
+
+public class AssignmentOperatorEx {
+
+	public static void main(String[] args) { // assignment operator
+		int a = 10;
+		int b = 20;
+		int c = 50;
+		int d = 10;
+		a += 100000; // a = a + 10
+		b += 50; // b = b+50
+		System.out.println(a);
+		System.out.println(b);
+		c -= 10; // c = c-10;
+		d -= 5; // d = d-5;
+		System.out.println(c);
+		System.out.println(d);
+		
+
+	}
+
+}
