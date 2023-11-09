69168334a199352b408ea640257991846b8b02fa
GouthamiVaddepalli <144352304+GouthamiVaddepalli@users.noreply.github.com>
2023-11-09 21:15:43 +0530

    Add files via upload

---
+++ ForloopEx.java
@@ -0,0 +1,21 @@
+package com.traines.corejava;
+
+public class ForloopEx {
+
+	public static void main(String[] args) {
+		for(int i = 1; i <=10; i++) {//2 3 
+			
+			System.out.println(i);//1 2 3 4 5 ...10
+			
+		}
+		System.out.println("***********************************");
+		
+		for (int i = 2; i <= 10; i = i + 2) { //4 6 
+			
+			
+			System.out.println(i);//2 4 6 ....10
+		}
+
+	}
+
+}
