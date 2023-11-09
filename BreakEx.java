c55c745024a9e83d9f3ad27bdf6b4ed9c2ac26ad
GouthamiVaddepalli <144352304+GouthamiVaddepalli@users.noreply.github.com>
2023-11-09 21:14:51 +0530

    Add files via upload

---
+++ BreakEx.java
@@ -0,0 +1,21 @@
+package com.traines.corejava;
+
+public class BreakEx {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <=10; i++) {
+			
+			if(i==6) { //true 
+				break;
+			}
+			
+			System.out.println(i); //1 2 3 4 5
+			
+		}
+		
+		System.out.println("*********************");
+
+	}
+
+}
