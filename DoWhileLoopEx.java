d9348ea000aa35f133aa5b4032247fbe0edc7d4a
GouthamiVaddepalli <144352304+GouthamiVaddepalli@users.noreply.github.com>
2023-11-09 21:15:28 +0530

    Add files via upload

---
+++ DoWhileLoopEx.java
@@ -0,0 +1,14 @@
+package com.traines.corejava;
+
+public class DoWhileLoopEx {
+
+	public static void main(String[] args) {
+		int i = 1;
+		do {
+			
+	     System.out.println(i); // 1 2 ----10
+	     i++;
+		}while(i <= 10);
+	}
+
+}
