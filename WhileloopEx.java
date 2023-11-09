498b9a9c1b278d52f67cc0dfe82b77ac18ff69e9
GouthamiVaddepalli <144352304+GouthamiVaddepalli@users.noreply.github.com>
2023-11-09 21:16:11 +0530

    Add files via upload

---
+++ WhileloopEx.java
@@ -0,0 +1,14 @@
+package com.traines.corejava;
+
+public class WhileloopEx {
+
+	public static void main(String[] args) {
+		int i = 2;
+		while (i <= 10) {
+			System.out.println(i); // 1 2
+			i = i + 2;// 2 3 4.........9 10
+
+		}
+	}
+
+}
