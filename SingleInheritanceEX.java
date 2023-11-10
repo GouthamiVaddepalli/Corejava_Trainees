b3c7588df42b706147d97ce07f55241c6c79fad8
GouthamiVaddepalli <144352304+GouthamiVaddepalli@users.noreply.github.com>
2023-11-10 20:49:27 +0530

    Add files via upload

---
+++ SingleInheritanceEX.java
@@ -0,0 +1,29 @@
+package com.corejava.oops;
+
+class Parent{
+	int marks = 100;
+    public void display() {
+    	
+    	System.out.println("This is from a parent class");
+    }
+	
+	
+}
+
+public class SingleInheritanceEX  extends Parent{
+	void displayMsg() {
+		
+		System.out.println("This is from a child class");
+	}
+
+	public static void main(String[] args) {
+		
+		SingleInheritanceEX obj = new SingleInheritanceEX();
+		
+		obj.displayMsg();
+
+		obj.display();
+		System.out.println("marks: " + obj.marks);
+	}
+
+}
