ee90380e094201e80773bf8f67bdf0ba0c8922a2
GouthamiVaddepalli <144352304+GouthamiVaddepalli@users.noreply.github.com>
2023-11-22 21:12:28 +0530

    Add files via upload

---
+++ ConstructorEx.java
@@ -0,0 +1,14 @@
+package com.corejava.oops;
+
+public class ConstructorEx {
+	int b ;
+	String name;
+
+	public static void main(String[] args) {
+		ConstructorEx cons = new ConstructorEx();// default constructor
+		System.out.println(cons.b);
+		System.out.println(cons.name); // null
+		
+	}
+
+}
