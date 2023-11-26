f14c474733ae3fa16142b9a72981e6ef0b827a10
GouthamiVaddepalli <144352304+GouthamiVaddepalli@users.noreply.github.com>
2023-11-26 20:46:12 +0530

    Add files via upload

---
+++ Protected1.java
@@ -0,0 +1,21 @@
+package com.corejava.oops;
+
+class Protected2 {
+	protected void hello() {
+		System.out.println("this is protected method from protected2 class");
+	}
+}
+
+public class Protected1 extends Protected2 {
+	protected int marks = 100;
+
+	public static void main(String[] args) {
+		Protected1 pro1 = new Protected1();
+		System.out.println(pro1.marks);
+		pro1.hello();
+		Protected2 pro2 = new Protected2();
+		pro2.hello();
+
+	}
+
+}
