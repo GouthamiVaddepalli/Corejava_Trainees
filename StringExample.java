9f4bcafde81db21b2b60e3dbe83a529e2cda0bdb
GouthamiVaddepalli <144352304+GouthamiVaddepalli@users.noreply.github.com>
2023-11-29 21:00:30 +0530

    Add files via upload

---
+++ StringExample.java
@@ -0,0 +1,17 @@
+package com.corejava.oops;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		String name = "welcome";
+		//System.out.println(name.concat("to india"));
+	    
+        String result = name.concat("to String"); // welcometo String
+        System.out.println(result);
+        
+		
+		
+		
+	}
+
+}
