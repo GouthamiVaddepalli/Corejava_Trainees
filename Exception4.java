0ee6d9805df3814df0171640f4dcdbbf570b7be0
GouthamiVaddepalli <144352304+GouthamiVaddepalli@users.noreply.github.com>
2023-12-06 21:00:00 +0530

    Add files via upload

--- Exception4.java
+++ Exception4.java
@@ -4,18 +4,32 @@ public class Exception4 {
 
 	public static void main(String[] args) {
 		try {
-			int x = 12 / 3;
+			
+			int x = 12/0;
+			try {
+				int b = 40/2;
+				try {
+					int a[] = new int[5];
+					a[5] = 19;
+				}finally {
+					System.out.println("inner try block");
+				}
+				System.out.println("remaining try statements");
+			}catch(ArithmeticException e) {
+				System.out.println("enter denomintor as zero");
+			}catch(ArrayIndexOutOfBoundsException ae) {
+				System.out.println("max index is 4");
+			}
 		}
-	
-		catch (Exception e) {
-			System.out.println("handles all the exceptions");
-
-		} finally {
-			System.out.println("executes everytime");
-
+		catch(Exception e) {
+			System.out.println("handles all execeptions");
 		}
-	
-		System.out.println("remaninig lines of code");
+		finally {
+			System.out.println("Executes always ");
+			
+		}
+		System.out.println("remaining lines of code ");
+		
 	}
 			
 }
