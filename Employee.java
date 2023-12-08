29fdbd8658b695e4d9f91f0712ca0e5cdffb3950
GouthamiVaddepalli <144352304+GouthamiVaddepalli@users.noreply.github.com>
2023-12-08 21:31:54 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,50 @@
+package com.corejava.files;
+
+import java.io.Serializable;
+
+public class Employee  implements Serializable{
+	private int empId;
+	private String empName;
+	private int empSal;
+	private String empDesg;
+	public int getEmpId() {
+		return empId;
+	}
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public int getEmpSal() {
+		return empSal;
+	}
+	public void setEmpSal(int empSal) {
+		this.empSal = empSal;
+	}
+	public String getEmpDesg() {
+		return empDesg;
+	}
+	public void setEmpDesg(String empDesg) {
+		this.empDesg = empDesg;
+	}
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empSal=" + empSal + ", empDesg=" + empDesg
+				+ "]";
+	}
+	public Employee(int empId, String empName, int empSal, String empDesg) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.empSal = empSal;
+		this.empDesg = empDesg;
+	}
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
