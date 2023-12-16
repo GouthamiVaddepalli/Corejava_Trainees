372232804bf227576a7765b69d20991366d7a8a7
GouthamiVaddepalli <144352304+GouthamiVaddepalli@users.noreply.github.com>
2023-12-16 21:10:37 +0530

    Add files via upload

---
+++ HashSetEx.java
@@ -0,0 +1,26 @@
+package com.corejava.collections;
+
+import java.util.HashSet;
+
+public class HashSetEx {
+  // duplictes are not allowed
+	// insertion order is not preserved
+	public static void main(String[] args) {
+		HashSet hs = new HashSet();
+		hs.add("Gouthami");
+		hs.add("Anjali");
+		hs.add("Gouthami");
+		hs.add(false);
+		hs.add(12);
+		System.out.println(hs);
+		
+		
+		
+		HashSet <String>  hs1 = new HashSet();
+		hs1.add("gouthami");
+		hs1.add("anjali");
+		hs1.add("gouthami");
+		System.out.println(hs1);
+	}
+
+}
