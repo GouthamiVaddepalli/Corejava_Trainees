5d3b3a3f0dc0b378864a3ecdd8d7121c2ed03fa1
GouthamiVaddepalli <144352304+GouthamiVaddepalli@users.noreply.github.com>
2024-01-01 21:18:05 +0530

    Add files via upload

---
+++ LocalDateEx.java
@@ -0,0 +1,24 @@
+package com.corejava.java8;
+
+import java.time.LocalDate;
+
+public class LocalDateEx {
+
+	public static void main(String[] args) {
+		LocalDate date = LocalDate.now();
+		System.out.println(date);
+		LocalDate yesterday = date.minusDays(1);
+		System.out.println("Yesterday date is : " + yesterday);
+		LocalDate tomorrow = date.plusDays(1);
+		System.out.println("tomorrow date is : " + tomorrow);
+		LocalDate nextmonth = date.plusMonths(1);
+		System.out.println("nextmonth is : " + nextmonth);
+		System.out.println("*****************************");
+		int dd = date.getDayOfMonth();
+		int mm = date.getMonthValue();
+		int yy = date.getYear();
+        System.out.println(dd + " " + mm + " " + yy);
+		
+	}
+
+}
